d5990095b2983f08199b8b0cbcbce5ae240b37bd
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-02 21:04:05 +0700

    Пример использования instanceKeeper

--- app/src/main/java/com/elmirov/mvidecompose/presentation/list/DefaultContactListComponent.kt
+++ app/src/main/java/com/elmirov/mvidecompose/presentation/list/DefaultContactListComponent.kt
@@ -1,24 +1,30 @@
 package com.elmirov.mvidecompose.presentation.list
 
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import com.arkivanov.essenty.instancekeeper.getOrCreate
 import com.elmirov.mvidecompose.data.RepositoryImpl
 import com.elmirov.mvidecompose.domain.entity.Contact
 import com.elmirov.mvidecompose.domain.usecase.GetContactsUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import com.elmirov.mvidecompose.util.componentScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class DefaultContactListComponent(
+    componentContext: ComponentContext,
     val onEditingContactRequest: (Contact) -> Unit,
     val onAddContactRequest: () -> Unit,
-) : ContactListComponent {
+) : ContactListComponent, ComponentContext by componentContext {
+
+    private val viewModel = instanceKeeper.getOrCreate { FakeViewModel() }
 
     private val repository = RepositoryImpl
     private val getContactsUseCase = GetContactsUseCase(repository)
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+    private val coroutineScope = componentScope()
+
     override val model: StateFlow<ContactListComponent.Model> = getContactsUseCase()
         .map { ContactListComponent.Model(it) }
         .stateIn(
@@ -34,4 +40,8 @@ class DefaultContactListComponent(
     override fun onAddContactClick() {
         onAddContactRequest()
     }
+}
+
+private class FakeViewModel() : InstanceKeeper.Instance {
+
 }

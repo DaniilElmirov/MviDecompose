25803bf3e626079b5d990ddf91f226195b2614a3
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-03 19:57:39 +0700

    Добавил разметку главного экрана

--- app/src/main/java/com/elmirov/mvidecompose/MainActivity.kt
+++ app/src/main/java/com/elmirov/mvidecompose/MainActivity.kt
@@ -3,62 +3,15 @@ package com.elmirov.mvidecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.elmirov.mvidecompose.domain.entity.Contact
-import com.elmirov.mvidecompose.ui.content.AddContact
-import com.elmirov.mvidecompose.ui.content.Contacts
-import com.elmirov.mvidecompose.ui.content.EditContact
-import com.elmirov.mvidecompose.ui.theme.MviDecomposeTheme
+import com.arkivanov.decompose.defaultComponentContext
+import com.elmirov.mvidecompose.presentation.root.DefaultRootComponent
+import com.elmirov.mvidecompose.ui.content.RootContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var screen by remember {
-                mutableStateOf<Screen>(Screen.ContactList)
-            }
-
-            MviDecomposeTheme {
-                when (val currentScreen = screen) {
-                    Screen.AddContact -> {
-                        AddContact(
-                            onContactSaved = {
-                                screen = Screen.ContactList
-                            }
-                        )
-                    }
-
-                    Screen.ContactList -> {
-                        Contacts(
-                            onAddContactClick = {
-                                screen = Screen.AddContact
-                            },
-                            onContactClick = {
-                                screen = Screen.EditContact(it)
-                            }
-                        )
-                    }
-
-                    is Screen.EditContact -> {
-                        EditContact(
-                            contact = currentScreen.contact,
-                            onContactChanged = {
-                                screen = Screen.ContactList
-                            }
-                        )
-                    }
-                }
-            }
+            RootContent(component = DefaultRootComponent(defaultComponentContext()))
         }
     }
-}
-
-sealed interface Screen {
-    data object ContactList : Screen
-    data object AddContact : Screen
-
-    data class EditContact(val contact: Contact) : Screen
 }

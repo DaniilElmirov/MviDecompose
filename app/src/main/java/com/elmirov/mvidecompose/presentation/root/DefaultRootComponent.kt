76a4ccedfd6ea1a2f334944ed71cdd75c9feb03e
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-03 19:35:36 +0700

    ChildStack и Value для навигации в decompose

--- app/src/main/java/com/elmirov/mvidecompose/presentation/root/DefaultRootComponent.kt
+++ app/src/main/java/com/elmirov/mvidecompose/presentation/root/DefaultRootComponent.kt
@@ -2,9 +2,12 @@ package com.elmirov.mvidecompose.presentation.root
 
 import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
+import com.arkivanov.decompose.value.Value
 import com.elmirov.mvidecompose.domain.entity.Contact
 import com.elmirov.mvidecompose.presentation.add.DefaultAddContactComponent
 import com.elmirov.mvidecompose.presentation.edit.DefaultEditContactComponent
@@ -18,11 +21,18 @@ class DefaultRootComponent(
     componentContext: ComponentContext,
 ) : RootComponent, ComponentContext by componentContext {
 
-    val navigation = StackNavigation<Config>()
+    private val navigation = StackNavigation<Config>()
 
-    fun child(
-        componentContext: ComponentContext,
+    val stack: Value<ChildStack<Config, ComponentContext>> = childStack(
+        source = navigation,
+        initialConfiguration = ContactList,
+        handleBackButton = true,
+        childFactory = ::child
+    )
+
+    private fun child(
         config: Config,
+        componentContext: ComponentContext,
     ): ComponentContext =
         when (config) {
             AddContact -> DefaultAddContactComponent(

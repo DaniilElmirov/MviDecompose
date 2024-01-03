92bb53c1a1e73327e299292b356c47f76ada5472
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-03 21:14:57 +0700

    Добавил store для экрана добавления контакта

---
+++ app/src/main/java/com/elmirov/mvidecompose/presentation/add/AddContactStore.kt
@@ -0,0 +1,24 @@
+package com.elmirov.mvidecompose.presentation.add
+
+import com.arkivanov.mvikotlin.core.store.Store
+
+interface AddContactStore :
+    Store<AddContactStore.Intent, AddContactStore.State, AddContactStore.Label> {
+
+    data class State(
+        val username: String,
+        val phone: String,
+    )
+
+    sealed interface Label {
+        data object ContactSaved : Label
+    }
+
+    sealed interface Intent {
+        data class ChangeUsername(val username: String) : Intent
+
+        data class ChangePhone(val phone: String) : Intent
+
+        data object SaveContact : Intent
+    }
+}

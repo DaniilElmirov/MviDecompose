7cd8623d330462fecc78e120f83199d649d110ad
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-04 16:56:34 +0700

    Добавил фабрику для stor'а добавления контакта

---
+++ app/src/main/java/com/elmirov/mvidecompose/presentation/add/AddContactStoreFactory.kt
@@ -0,0 +1,57 @@
+package com.elmirov.mvidecompose.presentation.add
+
+import com.arkivanov.mvikotlin.core.store.Reducer
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import com.elmirov.mvidecompose.domain.usecase.AddContactUseCase
+
+class AddContactStoreFactory(
+    private val storeFactory: StoreFactory,
+    private val addContactUseCase: AddContactUseCase,
+) {
+
+    fun create(): AddContactStore = object : AddContactStore,
+        Store<AddContactStore.Intent, AddContactStore.State, AddContactStore.Label> by storeFactory.create(
+            name = "AddContactStore",
+            initialState = AddContactStore.State(username = "", phone = ""),
+            reducer = ReducerImpl,
+            executorFactory = ::ExecutorImpl
+        ) {}
+
+    private sealed interface Action
+
+    private sealed interface Msg {
+        data class ChangeUsername(val username: String) : Msg
+
+        data class ChangePhone(val phone: String) : Msg
+    }
+
+    private object ReducerImpl : Reducer<AddContactStore.State, Msg> {
+        override fun AddContactStore.State.reduce(msg: Msg): AddContactStore.State = when (msg) {
+            is Msg.ChangePhone -> copy(phone = msg.phone)
+
+            is Msg.ChangeUsername -> copy(username = msg.username)
+        }
+    }
+
+    private inner class ExecutorImpl :
+        CoroutineExecutor<AddContactStore.Intent, Action, AddContactStore.State, Msg, AddContactStore.Label>() {
+        override fun executeIntent(
+            intent: AddContactStore.Intent,
+            getState: () -> AddContactStore.State,
+        ) {
+            when (intent) {
+                is AddContactStore.Intent.ChangePhone -> dispatch(Msg.ChangePhone(intent.phone))
+
+                is AddContactStore.Intent.ChangeUsername -> dispatch(Msg.ChangeUsername(intent.username))
+
+                AddContactStore.Intent.SaveContact -> {
+                    val state = getState()
+                    addContactUseCase(username = state.username, phone = state.phone)
+                    publish(AddContactStore.Label.ContactSaved)
+                }
+            }
+        }
+    }
+}

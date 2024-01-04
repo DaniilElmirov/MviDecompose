4a22b340b6a986d0a20178bf5b0d092d2846b266
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-04 19:42:18 +0700

    Использование шаблона mvisc

---
+++ app/src/main/java/com/elmirov/mvidecompose/sample/TestStore.kt
@@ -0,0 +1,65 @@
+package com.elmirov.mvidecompose.sample
+
+import com.arkivanov.mvikotlin.core.store.Reducer
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineBootstrapper
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import com.elmirov.mvidecompose.sample.TestStore.Intent
+import com.elmirov.mvidecompose.sample.TestStore.Label
+import com.elmirov.mvidecompose.sample.TestStore.State
+
+internal interface TestStore : Store<Intent, State, Label> {
+
+    sealed interface Intent {
+    }
+
+    data class State(
+        val test: String = "test"
+    )
+
+    sealed interface Label {
+    }
+}
+
+internal class TestStoreFactory(
+    private val storeFactory: StoreFactory
+) {
+
+    fun create(): TestStore =
+        object : TestStore, Store<Intent, State, Label> by storeFactory.create(
+            name = "TestStore",
+            initialState = State(),
+            bootstrapper = BootstrapperImpl(),
+            executorFactory = ::ExecutorImpl,
+            reducer = ReducerImpl
+        ) {}
+
+    private sealed interface Action {
+    }
+
+    private sealed interface Msg {
+    }
+
+    private class BootstrapperImpl : CoroutineBootstrapper<Action>() {
+        override fun invoke() {
+        }
+    }
+
+    private class ExecutorImpl : CoroutineExecutor<Intent, Action, State, Msg, Label>() {
+        override fun executeIntent(intent: Intent, getState: () -> State) {
+        }
+
+        override fun executeAction(action: Action, getState: () -> State) {
+        }
+    }
+
+    private object ReducerImpl : Reducer<State, Msg> {
+        override fun State.reduce(message: Msg): State =
+            when (message) {
+                else -> {
+                    State()
+                }
+            }
+    }
+}

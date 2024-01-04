abd1c6bf9b3a6f98b5ae35394fd6b9918cdeda15
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-04 16:57:46 +0700

    Добавил фабрику для stor'а изменения контакта

---
+++ app/src/main/java/com/elmirov/mvidecompose/presentation/edit/EditContactStoreFactory.kt
@@ -0,0 +1,69 @@
+package com.elmirov.mvidecompose.presentation.edit
+
+import com.arkivanov.mvikotlin.core.store.Reducer
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import com.elmirov.mvidecompose.domain.entity.Contact
+import com.elmirov.mvidecompose.domain.usecase.EditContactUseCase
+
+class EditContactStoreFactory(
+    private val storeFactory: StoreFactory,
+    private val editContactUseCase: EditContactUseCase,
+) {
+
+    fun create(contact: Contact): EditContactStore = object : EditContactStore,
+        Store<EditContactStore.Intent, EditContactStore.State, EditContactStore.Label> by storeFactory.create(
+            name = "EditContactStore",
+            initialState = EditContactStore.State(
+                id = contact.id,
+                username = contact.username,
+                phone = contact.phone,
+            ),
+            reducer = ReducerImpl,
+            executorFactory = ::ExecutorImpl
+        ) {}
+
+    private sealed interface Action
+
+    private sealed interface Msg {
+        data class ChangeUsername(val username: String) : Msg
+
+        data class ChangePhone(val phone: String) : Msg
+    }
+
+    private object ReducerImpl : Reducer<EditContactStore.State, Msg> {
+        override fun EditContactStore.State.reduce(msg: Msg): EditContactStore.State = when (msg) {
+            is Msg.ChangePhone -> copy(phone = msg.phone)
+
+            is Msg.ChangeUsername -> copy(username = msg.username)
+        }
+    }
+
+    private inner class ExecutorImpl :
+        CoroutineExecutor<EditContactStore.Intent, Action, EditContactStore.State, Msg, EditContactStore.Label>() {
+        override fun executeIntent(
+            intent: EditContactStore.Intent,
+            getState: () -> EditContactStore.State,
+        ) {
+            when (intent) {
+                is EditContactStore.Intent.ChangePhone -> dispatch(Msg.ChangePhone(intent.phone))
+
+                is EditContactStore.Intent.ChangeUsername -> dispatch(Msg.ChangeUsername(intent.username))
+
+                EditContactStore.Intent.SaveContact -> {
+                    val state = getState()
+
+                    val contact = Contact(
+                        id = state.id,
+                        username = state.username,
+                        phone = state.phone
+                    )
+                    editContactUseCase(contact)
+
+                    publish(EditContactStore.Label.ContactSaved)
+                }
+            }
+        }
+    }
+}
